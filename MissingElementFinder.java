ebedcb3c0203d8ed3428934177662a75e3256479
anil_akbay <anilakbay20@gmail.com>
2024-11-25 15:13:27 +0300

    Eksik sayı tespiti ve hata yönetimi iyileştirildi
    - Rastgele bir sayı silindikten sonra eksik sayıyı tespit etmek için yeni bir metod eklendi (`removeRandomNumberAndGet()`).
    - Eksik sayı bulunamadığında özel bir istisna sınıfı olan `MissingElementException` ile hata yönetimi sağlandı.
    - Kod yapısı daha modüler hale getirildi, açıklamalarla birlikte anlaşılabilirlik artırıldı.
    - Logger kullanılarak işlem süreci detaylı şekilde ekrana yazdırıldı.

--- MissingElementFinder.java
+++ MissingElementFinder.java
@@ -8,32 +8,51 @@ public class MissingElementFinder {
     private static final Logger logger = Logger.getLogger(MissingElementFinder.class.getName());
 
     public static void main(String[] args) {
+        // 100 farklı sayıyı üret
         Set<Integer> originalNumbers = generateRandomNumbers();
+        
+        // Kopya listeyi oluştur ve rastgele bir sayı sil
         Set<Integer> copiedNumbers = new HashSet<>(originalNumbers);
-
-        // Rastgele bir sayı seç ve kopya listeden sil
-        int randomNumber = new Random().nextInt(100);
-        logger.info("Random sayı: " + randomNumber);
-        copiedNumbers.remove(randomNumber);
-
+        int missingNumber = removeRandomNumberAndGet(originalNumbers, copiedNumbers);
+        
         // Eksik sayıyı bul ve yazdır
-        logger.info("Eksik Sayı: " + findMissingElement(originalNumbers, copiedNumbers));
+        logger.info("Eksik Sayı: " + missingNumber);
     }
 
+    // Rastgele 100 farklı sayı üretir
     private static Set<Integer> generateRandomNumbers() {
         Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        
+        // 1 ile 100 arasında 100 farklı sayı üret
         while (numbers.size() < 100) {
-            numbers.add(new Random().nextInt(100) + 1);
+            numbers.add(random.nextInt(100) + 1);
         }
         return numbers;
     }
 
+    // Rastgele bir sayıyı kopya listeden siler ve eksik sayıyı döndürür
+    private static int removeRandomNumberAndGet(Set<Integer> original, Set<Integer> copied) {
+        int randomNumber = new Random().nextInt(100) + 1;
+        logger.info("Random sayı: " + randomNumber);
+        copied.remove(randomNumber);
+        return findMissingElement(original, copied);
+    }
+
+    // Eksik sayıyı bulur
     private static int findMissingElement(Set<Integer> original, Set<Integer> copied) {
         for (int num : original) {
             if (!copied.contains(num)) {
-                return num;
+                return num;  // Eksik sayıyı döndür
             }
         }
-        throw new RuntimeException("Eksik sayı bulunamadı!");
+        throw new MissingElementException("Eksik sayı bulunamadı!"); // Hata durumunda özel istisna fırlat
+    }
+}
+
+// Eksik sayıyı bulamadığında fırlatılacak özel istisna
+class MissingElementException extends RuntimeException {
+    public MissingElementException(String message) {
+        super(message);
     }
 }
